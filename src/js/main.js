f8781334961f7d630e483c6fc083c3a62873d834
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-11-05 22:31:44 +0100

    feat: many changes and styled gallery on first site

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,29 @@
 "use strict";
 console.log(`Hello world!`);
 
-console.log(location.pathname);
+// const galleryPhoto = document.querySelectorAll('.content__gallery--photo');
+// const galleryTitle = document.querySelectorAll('.gallery__title');
 
-const actualSubsite = document.querySelector('.nav__button--js');
+// galleryPhoto.forEach(title => title.addEventListener('mouseover', (e) => {
+//     galleryTitle.forEach(element => element.classList.toggle('gallery__title--visible')
+// )}));
+
+// galleryPhoto.forEach(title => title.addEventListener('mouseout', (e) => {
+//     galleryTitle.forEach(title => title.classList.toggle('gallery__title--visible')
+// )}));
+
+// galleryPhoto.forEach(addEventListener('mouseout', (e) => {
+//     galleryTitle.forEach(classList.remove('gallery__title--visible'))
+// }));
+
+
+
+
+// galleryPhoto.addEventListener('mouseover', (e) => {
+//     galleryTitle.classList.toggle('gallery__title--visible')
+// });
+
+// galleryPhoto.addEventListener('mouseout', (e) => {
+//     galleryTitle.classList.remove('gallery__title--visible')
+// });
 
