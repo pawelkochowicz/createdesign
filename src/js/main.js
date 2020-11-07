f2310c6cb7fc6ea383989ae52bd277f2665d5bd7
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-11-07 23:04:08 +0100

    feat: create realizations subsite with links to galleries plus few little changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,3 @@
 "use strict";
 console.log(`Hello world!`);
 
-// const galleryPhoto = document.querySelectorAll('.content__gallery--photo');
-// const galleryTitle = document.querySelectorAll('.gallery__title');
-
-// galleryPhoto.forEach(title => title.addEventListener('mouseover', (e) => {
-//     galleryTitle.forEach(element => element.classList.toggle('gallery__title--visible')
-// )}));
-
-// galleryPhoto.forEach(title => title.addEventListener('mouseout', (e) => {
-//     galleryTitle.forEach(title => title.classList.toggle('gallery__title--visible')
-// )}));
-
-// galleryPhoto.forEach(addEventListener('mouseout', (e) => {
-//     galleryTitle.forEach(classList.remove('gallery__title--visible'))
-// }));
-
-
-
-
-// galleryPhoto.addEventListener('mouseover', (e) => {
-//     galleryTitle.classList.toggle('gallery__title--visible')
-// });
-
-// galleryPhoto.addEventListener('mouseout', (e) => {
-//     galleryTitle.classList.remove('gallery__title--visible')
-// });
-
