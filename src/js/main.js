7829cfd7b032f3cd49a0808ced0bc8bf39f3cf0b
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-11-08 20:55:39 +0100

    feat: add hamburger menu (not work to good yet :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 "use strict";
 console.log(`Hello world!`);
 
+const navigationSwitcher = document.querySelector('.nav__switcher--js')
+navigationSwitcher.addEventListener('click',(e) => {
+    const navigationList = document.querySelector('.nav__menu--js')
+    navigationList.classList.toggle('nav__menu--visible')
+});
